da1c4b699f103d573f0d0c2f43412cc05a9711b5
Ucer <FayozbekJumabekov>
2022-03-05 00:31:54 +0500

    update image

--- lib/pages/detail_page.dart
+++ lib/pages/detail_page.dart
@@ -13,7 +13,8 @@ import '../services/store_service.dart';
 class DetailPage extends StatefulWidget {
   final Note? note;
   final String? noteKey;
-   const DetailPage({this.note,this.noteKey,Key? key}) : super(key: key);
+
+  const DetailPage({this.note, this.noteKey, Key? key}) : super(key: key);
   static const String id = "detail_page";
 
   @override
@@ -21,77 +22,83 @@ class DetailPage extends StatefulWidget {
 }
 
 class _DetailPageState extends State<DetailPage> {
-
   TextEditingController titleController = TextEditingController();
   TextEditingController contentController = TextEditingController();
-  DateTime? currentBackPressTime;
   bool isLoading = false;
   File? _image;
   final picker = ImagePicker();
 
-  void saveOrEditNote(){
-    if(widget.note == null){
-      String title = titleController.text.toString().trim();
-      String content = contentController.text.toString().trim();
-      if(_image == null) {
-        Log.i("Image not added");
-        saveNoteWithoutImage(title, content);
-      }
-       else {
-        uploadImage(title, content);
-      }
-    }
-
-    else{
-      editNote();
+  void saveOrEditNote() {
+    String title = titleController.text.toString().trim();
+    String content = contentController.text.toString().trim();
+    if (widget.note == null) {
+      addNote(title, content);
+    } else {
+      editNote(title, content);
     }
   }
 
-  /// Upload Image
-  void uploadImage(String title, String content) {
-    setState(() { isLoading = true;});
-    StoreService.uploadImage(_image!).then((imgUrl) => {
-      saveNoteWithImage(title, content, imgUrl),
-    });
-  }
-
-  /// Save not Without Image
-
-  void saveNoteWithoutImage(String title,String content)async{
-    String id = await HiveService.loadUserId(StorageKeys.UID);
-    Note note = Note(userId: id,title: title, content: content,image: null);
-    RTDBService.addNote(note).then((value) {
-      Log.i('Successfully added Note');
-      Navigator.pop(context,true);
-    });
-  }
-
-  /// Save Note With Image
-  void saveNoteWithImage(String title,String content,String? image)async{
-      String id = await HiveService.loadUserId(StorageKeys.UID);
-      Note note = Note(userId: id,title: title, content: content,image: image);
-      RTDBService.addNote(note).then((value) {
-        setState(() { isLoading = false;});
-        Log.i('Successfully added');
-        Navigator.pop(context,true);
+  /// Add Note
+  void addNote(String title, String content) {
+    if (_image != null) {
+      setState(() {
+        isLoading = true;
       });
+      StoreService.uploadImage(_image!).then((imgUrl) => {
+            saveNote(title, content, imgUrl),
+          });
+    } else {
+      saveNote(title, content, null);
+    }
   }
+
+  /// Save Note
+  void saveNote(String title, String content, String? image) async {
+    String id = await HiveService.loadUserId(StorageKeys.UID);
+    Note note = Note(userId: id, title: title, content: content, image: image);
+    RTDBService.addNote(note).then((value) {
+      setState(() {
+        isLoading = false;
+      });
+      Log.i('Successfully added');
+      Navigator.pop(context, true);
+    });
+  }
+
 
   /// Edit Note
-  void editNote(){
-    String title = titleController.text.trim().toString();
-    String content = contentController.text.trim().toString();
-    Note note = Note(userId: widget.note!.userId,title: title, content: content,image: widget.note!.image);
-    RTDBService.updateNoteWithKey(widget.noteKey!, note).then((value){
-      setState(() {isLoading = false;});
+  void editNote(String title, String content) {
+    if (_image != null) {
+      setState(() {
+        isLoading = true;
+      });
+      StoreService.uploadImage(_image!)
+          .then((imgUrl) => {updateNote(title, content, imgUrl)});
+    } else {
+      updateNote(title, content, widget.note!.image);
+    }
+  }
+
+  /// Update Note
+  void updateNote(String title, String content, String? image) {
+    Note note = Note(
+        userId: widget.note!.userId,
+        title: title,
+        content: content,
+        image: image);
+    RTDBService.updateNoteWithKey(widget.noteKey!, note).then((value) {
+      setState(() {
+        isLoading = false;
+      });
       Log.i('Successfully Edited');
-      Navigator.pop(context,true);
+      Navigator.pop(context, true);
     });
   }
+
 
   /// Select Save or Edit functions
   void loadNote(Note? note) {
-    if(note != null) {
+    if (note != null) {
       setState(() {
         titleController.text = note.title!;
         contentController.text = note.content!;
@@ -113,19 +120,20 @@ class _DetailPageState extends State<DetailPage> {
 
   @override
   void initState() {
-
     super.initState();
     loadNote(widget.note);
   }
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       backgroundColor: Colors.white,
+
       /// Appbar
       appBar: AppBar(
         backgroundColor: Colors.white,
         elevation: 0,
+
         /// Back Button
         leading: IconButton(
           icon: const Icon(
@@ -137,7 +145,6 @@ class _DetailPageState extends State<DetailPage> {
           },
         ),
         actions: [
-
           /// Save Button
           TextButton(
               onPressed: () {
@@ -145,27 +152,30 @@ class _DetailPageState extends State<DetailPage> {
               },
               child: const Text(
                 "Save",
-                style: TextStyle(color: Colors.black,fontSize: 18),
+                style: TextStyle(color: Colors.black, fontSize: 18),
               ))
         ],
       ),
+
       /// Body
       body: SingleChildScrollView(
         child: Container(
           width: MediaQuery.of(context).size.width,
-          padding: const EdgeInsets.symmetric(horizontal: 20,vertical: 10),
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.start,
             children: [
-
               /// Add title
               TextField(
                 controller: titleController,
                 cursorColor: Colors.amber,
                 cursorHeight: 30,
                 maxLines: null,
-
-                style: const TextStyle(fontSize: 25, fontWeight: FontWeight.w600,color: Colors.black,),
+                style: const TextStyle(
+                  fontSize: 25,
+                  fontWeight: FontWeight.w600,
+                  color: Colors.black,
+                ),
                 decoration: const InputDecoration(
                   isCollapsed: true,
                   hintText: " Title",
@@ -186,7 +196,10 @@ class _DetailPageState extends State<DetailPage> {
                 cursorColor: Colors.amber,
                 cursorHeight: 22,
                 maxLines: null,
-                style: const TextStyle(fontSize: 16,color: Colors.black,),
+                style: const TextStyle(
+                  fontSize: 16,
+                  color: Colors.black,
+                ),
                 decoration: const InputDecoration(
                   isCollapsed: true,
                   hintText: "  Start typing...",
@@ -198,24 +211,47 @@ class _DetailPageState extends State<DetailPage> {
                 ),
               ),
 
+              const SizedBox(
+                height: 20,
+              ),
               /// Add image
-              const SizedBox(height: 20,),
               GestureDetector(
-                  onTap: (){
+                  onTap: () {
                     _getImage();
                   },
-                  child: (_image != null) ? Image.file(_image!,height: 300,width: 400,fit: BoxFit.cover,) :Image(image: AssetImage('assets/images/im_upload.png'),width: 200,height: 150,)),
-              Text("Click for upload image",style: TextStyle(fontSize: 16,fontWeight: FontWeight.bold),),
+                  child: ((widget.note != null) && (_image == null))
+                      ? Image.network(
+                          widget.note!.image!,
+                          height: 300,
+                          width: 400,
+                          fit: BoxFit.cover,
+                        )
+                      : (_image != null)
+                          ? Image.file(
+                              _image!,
+                              height: 300,
+                              width: 400,
+                              fit: BoxFit.cover,
+                            )
+                          : const Image(
+                              image: AssetImage('assets/images/im_upload.png'),
+                              width: 200,
+                              height: 150,
+                            )),
+               Text(
+                (widget.note != null) ? "Click above image for update":"Click above image for add",
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              ),
               (isLoading)
                   ? Center(
-                child: Lottie.asset('assets/anims/loading.json', width: 200),
-              ) : SizedBox.shrink()
-
+                      child:
+                          Lottie.asset('assets/anims/loading.json', width: 200),
+                    )
+                  : SizedBox.shrink()
             ],
           ),
         ),
       ),
     );
   }
-
 }
